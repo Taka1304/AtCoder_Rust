9f8425654d2d5e555a1e691b3d8535de5d341b47
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-04-16 22:49:40 +0900

    refactor: itertoolsで可読性を上げる

--- src/goldbach_conjecture.rs
+++ src/goldbach_conjecture.rs
@@ -1,119 +1,50 @@
-use primes::{Sieve, PrimeSet};
+use itertools::Itertools;
+use primes::{PrimeSet, Sieve};
 
-pub fn main(which_conjecture: u64) {
-  let limit: u64 = 30;
-
-  match which_conjecture {
-    1 => {
-      println!("Testing the Strong Goldbach Conjecture...");
-      for n in (4..=limit).step_by(2) {
-        if !strong_goldbach(n) {
-          println!("Failed at {}", n);
-          continue;
-        }
-      }
-    }
-    2 => {
-      println!("Testing the Weak Goldbach Conjecture...");
-      for n in (9..=limit).step_by(2) {
-        if !weak_goldbach(n) {
-          println!("Failed at {}", n);
-          continue;
-        }
-      }
-    }
-    3 => {
-      println!("Testing the Extended Goldbach Conjecture...");
-      for n in 8..=limit {
-        if !extended_goldbach(n) {
-          println!("Failed at {}", n);
-          continue;
-        }
-      }
-    }
-    _ => println!("Invalid input!"),
-  }
-}
-
-fn strong_goldbach(n: u64) -> bool {
+pub fn strong_goldbach(n: u64) -> bool {
   let mut primes = Sieve::new();
-  let vec = primes.iter().take((n).try_into().unwrap()).collect::<Vec<_>>();
+  let vec = primes.iter().take_while(|&p| p <= n).collect::<Vec<_>>();
 
-  let mut result: Vec<(u64, u64)> = vec![];
+  let result = vec
+    .iter()
+    .combinations_with_replacement(2)
+    .filter(|comb| comb.iter().map(|&&p| p).sum::<u64>() == n)
+    .collect::<Vec<_>>();
 
-  for i in 0..vec.len() {
-    let p = vec[i];
-    if p > n / 2 {
-      break;
-    }
-    if p <= n && primes.is_prime(n - p) {
-      result.push((p, n - p));
-      continue;
-    }
-  }
   println!("{}: {:?}", n, result);
-  result.len() > 0
+  !result.is_empty()
 }
 
-fn weak_goldbach(n: u64) -> bool {
+pub fn weak_goldbach(n: u64) -> bool {
   let mut primes = Sieve::new();
-  let vec = primes.iter().take((n).try_into().unwrap()).collect::<Vec<_>>();
+  let vec = primes.iter().take_while(|&p| p <= n).collect::<Vec<_>>();
 
-  let mut result: Vec<(u64, u64, u64)> = vec![];
+  let result = vec
+    .iter()
+    .combinations_with_replacement(3)
+    .filter(|comb| comb.iter().map(|&&p| p).sum::<u64>() == n)
+    .collect::<Vec<_>>();
 
-  for i in 0..vec.len() {
-    let p = vec[i];
-    if p > n / 2 {
-      break;
-    }
-    for j in i..vec.len() {
-      let q = vec[j];
-      if p + q <= n && primes.is_prime(n - p - q) {
-        result.push((p, q, n - p - q));
-        break;
-      }
-    }
-  }
   println!("{}: {:?}", n, result);
-  result.len() > 0
+  !result.is_empty()
 }
 
-#[derive(Debug)]
-#[allow(dead_code)]
-enum GoldbachTuple {
-  Triple(u64, u64, u64),
-  Quadruple(u64, u64, u64, u64),
-}
-
-fn extended_goldbach(n: u64) -> bool {
+pub fn extended_goldbach(n: u64) -> bool {
   let mut primes = Sieve::new();
-  let vec = primes.iter().take((n).try_into().unwrap()).collect::<Vec<_>>();
+  let vec = primes.iter().take_while(|&p| p <= n).collect::<Vec<_>>();
 
-  let mut result: Vec<GoldbachTuple> = vec![];
+  let result_triples = vec
+    .iter()
+    .combinations_with_replacement(3)
+    .filter(|comb| comb.iter().map(|&&p| p).sum::<u64>() == n)
+    .collect::<Vec<_>>();
 
-  for i in 0..vec.len() {
-    let p = vec[i];
-    if p > n / 2 {
-      break;
-    }
-    for j in i..vec.len() {
-      let q = vec[j];
-      if p + q > n || q > n - p - q {
-        break;
-      } else if primes.is_prime(n - p - q) {
-        result.push(GoldbachTuple::Triple(p, q, n - p - q));
-      }
-      for k in j..vec.len() {
-        let r = vec[k];
-        if p + q + r > n || n - p - q - r < r {
-          break;
-        } else if primes.is_prime(n - p - q - r) {
-          result.push(GoldbachTuple::Quadruple(p, q, r, n - p - q - r));
-          break;
-        }
-      }
-    }
-  }
-  println!("{}: {:?}", n, result);
-  result.len() > 0
+  let result_quadruples = vec
+    .iter()
+    .combinations_with_replacement(4)
+    .filter(|comb| comb.iter().map(|&&p| p).sum::<u64>() == n)
+    .collect::<Vec<_>>();
+
+  println!("{}: {:?} {:?}", n, result_triples, result_quadruples);
+  !result_triples.is_empty() || !result_quadruples.is_empty()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod goldbach_conjecture;
-use std::time::Instant;
 use proconio::input;
+use std::time::Instant;
 
 fn main() {
   println!("1: Strong Goldbach Conjecture");
@@ -10,9 +10,41 @@ fn main() {
   input! {
     which_conjecture: u64,
   }
-
+  // 実行時間を計測
   let start = Instant::now();
-  goldbach_conjecture::main(which_conjecture);
+
+  let limit: u64 = 30;
+  match which_conjecture {
+    1 => {
+      println!("Testing the Strong Goldbach Conjecture...");
+      for n in (4..=limit).step_by(2) {
+        if !goldbach_conjecture::strong_goldbach(n) {
+          println!("Failed at {}", n);
+          continue;
+        }
+      }
+    }
+    2 => {
+      println!("Testing the Weak Goldbach Conjecture...");
+      for n in (9..=limit).step_by(2) {
+        if !goldbach_conjecture::weak_goldbach(n) {
+          println!("Failed at {}", n);
+          continue;
+        }
+      }
+    }
+    3 => {
+      println!("Testing the Extended Goldbach Conjecture...");
+      for n in 8..=limit {
+        if !goldbach_conjecture::extended_goldbach(n) {
+          println!("Failed at {}", n);
+          continue;
+        }
+      }
+    }
+    _ => println!("Invalid input!"),
+  }
+
   let duration = start.elapsed();
   println!("Time: {:?}", duration);
 }
