d03364b6f3e2fd57b79626f303b21929412a5b5c
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-04-18 17:07:53 +0900

    move: goldbach

--- src/goldbach_conjecture/mod.rs
+++ src/goldbach_conjecture/mod.rs
@@ -48,3 +48,53 @@ pub fn extended_goldbach(n: u64) -> bool {
   println!("{}: {:?} {:?}", n, result_triples, result_quadruples);
   !result_triples.is_empty() || !result_quadruples.is_empty()
 }
+
+// use proconio::input;
+// use std::time::Instant;
+
+// fn main() {
+//   println!("1: Strong Goldbach Conjecture");
+//   println!("2: Weak Goldbach Conjecture");
+//   println!("3: Extended Goldbach Conjecture");
+
+//   input! {
+//     which_conjecture: u64,
+//   }
+//   // 実行時間を計測
+//   let start = Instant::now();
+
+//   let limit: u64 = 50;
+//   match which_conjecture {
+//     1 => {
+//       println!("Testing the Strong Goldbach Conjecture...");
+//       for n in (4..=limit).step_by(2) {
+//         if !goldbach_conjecture::strong_goldbach(n) {
+//           println!("Failed at {}", n);
+//           continue;
+//         }
+//       }
+//     }
+//     2 => {
+//       println!("Testing the Weak Goldbach Conjecture...");
+//       for n in (7..=limit).step_by(2) {
+//         if !goldbach_conjecture::weak_goldbach(n) {
+//           println!("Failed at {}", n);
+//           continue;
+//         }
+//       }
+//     }
+//     3 => {
+//       println!("Testing the Extended Goldbach Conjecture...");
+//       for n in 7..=limit {
+//         if !goldbach_conjecture::extended_goldbach(n) {
+//           println!("Failed at {}", n);
+//           continue;
+//         }
+//       }
+//     }
+//     _ => println!("Invalid input!"),
+//   }
+
+//   let duration = start.elapsed();
+//   println!("Time: {:?}", duration);
+// }
